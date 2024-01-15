62698629fb9a66576488757ca70d47a46943a533
JarrettTo <justin.jarrett.to@gmail.com>
2024-01-15 17:54:23 +0800

    More Env fixes

--- pages/api/oauth/callback.ts
+++ pages/api/oauth/callback.ts
@@ -4,9 +4,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const code = req.query.code as string;
-    const clientID = 'zxopq57digvsdau';
-    const clientSecret = 'zek98gyqk99i51d';
-    const redirectURI = 'http://localhost:3000/api/oauth/callback';
+
+    const redirectURI = process.env.BASE_URL + '/api/oauth/callback';
 
     try {
         const params = new URLSearchParams();
@@ -14,7 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         params.append('grant_type', 'authorization_code');
         params.append('client_id', process.env.CLIENT_ID);
         params.append('client_secret', process.env.CLIENT_SECRET);
-        params.append('redirect_uri', process.env.REDIRECT_URI);
+        params.append('redirect_uri', redirectURI);
 
         const response = await axios.post('https://api.dropbox.com/oauth2/token', params, {
             headers: {
